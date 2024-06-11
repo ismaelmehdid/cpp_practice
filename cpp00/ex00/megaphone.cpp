4cea6aefca198ac789c387832cc3784b0ddcff1a
Ismaël MEHDID <ismaelmehdidwork@gmail.com>
2024-06-11 15:53:26 +0200

    fixed cpp00 ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,39 +1,21 @@
 #include <iostream>
 
-int mystrlen(char *str)
-{
-	if (str == NULL)
-	{
-		return 0;
-	}
-	int i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return i;
-}
-
 int main(int argc, char **argv)
 {
-	if (argc > 1)
+	if (argc <= 1)
 	{
-		for (int i = 1; i < argc; i++)
-		{
-			int length = mystrlen(argv[i]);
-			for (int j = 0; j < length; j++)
-			{
-				std::cout << (char)toupper(argv[i][j]);
-			}
-			if (i + 1 != argc)
-			{
-				std::cout << " ";
-			}
-		}
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << '\n';
+		return 0;
 	}
-	else
+	std::string str;
+	for (int i = 1; i < argc; i++)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		str = argv[i];
+		int length = str.size();
+		for (int j = 0; j < length; j++)
+		{
+			std::cout << (char)std::toupper(str.at(j));
+		}
 	}
 	std::cout << '\n';
 	return 0;
