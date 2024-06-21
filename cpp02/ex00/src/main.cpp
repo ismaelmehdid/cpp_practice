79d88267354a124229a3c2338afd6299b2a78be2
Ismaël MEHDID <ismaelmehdidwork@gmail.com>
2024-06-21 17:37:09 +0200

    added a test in ex00 of cpp02

--- cpp02/ex00/src/main.cpp
+++ cpp02/ex00/src/main.cpp
@@ -8,6 +8,7 @@ int main( void )
 	Fixed b( a );
 	Fixed c;
 	c = b;
+	a.setRawBits(42);
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
