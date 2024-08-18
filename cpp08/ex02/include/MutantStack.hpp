605e00d83267da6e02925758237c90b6b300e0ed
Ismaël MEHDID <ismaelmehdidwork@gmail.com>
2024-08-18 17:53:30 +0200

    finished cpp08

--- cpp08/ex02/include/MutantStack.hpp
+++ cpp08/ex02/include/MutantStack.hpp
@@ -1,13 +1,34 @@
 #pragma once
 
 #include <stack>
+#include <iostream>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
     public:
-        typedef typename std::deque<T>::iterator iterator;
-        typedef typename std::deque<T>::const_iterator const_iterator;
+
+        MutantStack() : std::stack<T>() {
+            std::cout << "A MutantStack has been created." << std::endl;
+        }
+
+        MutantStack(const MutantStack &toCopy) : std::stack<T>(toCopy) {
+            std::cout << "Copy constructor of MutantStack called." << std::endl;
+        }
+
+        MutantStack &operator=(const MutantStack &toCopy) {
+            if (this != &toCopy) {
+                std::stack<T>::operator=(toCopy);
+            }
+            return *this;
+        }
+
+        ~MutantStack() {
+            std::cout << "A MutantStack has been destroyed." << std::endl;
+        }
+
+        typedef typename std::deque<T>::iterator iterator;              // read and write
+        typedef typename std::deque<T>::const_iterator const_iterator;  // read only
 
         iterator begin() {
             return this->c.begin();
--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -4,6 +4,11 @@
 #include <list>
 int main()
 {
+    //===----------------------------------------------------------------------===//
+    std::cout << "\033[31m//===" << std::string(70, '-') << "===//\033[0m" << std::endl;
+    std::cout << "\033[31m//    Subject tests\033[0m" << std::endl;
+    std::cout << "\033[31m//===" << std::string(70, '-') << "===//\033[0m" << std::endl;
+    //===----------------------------------------------------------------------===//
     {
         MutantStack<int> mstack;
         mstack.push(5);
@@ -27,5 +32,85 @@ int main()
         std::stack<int> s(mstack);
     }
     std::cout << std::endl;
+    {
+        std::list<int> mstack;
+        mstack.push_back(5);
+        mstack.push_back(17);
+        std::cout << mstack.back() << std::endl;
+        mstack.pop_back();
+        std::cout << mstack.size() << std::endl;
+        mstack.push_back(3);
+        mstack.push_back(5);
+        mstack.push_back(737);
+        mstack.push_back(0);
+        std::list<int>::iterator it = mstack.begin();
+        std::list<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::list<int> s(mstack);
+    }
+    //===----------------------------------------------------------------------===//
+    std::cout << "\033[31m//===" << std::string(70, '-') << "===//\033[0m" << std::endl;
+    std::cout << "\033[31m//    My tests\033[0m" << std::endl;
+    std::cout << "\033[31m//===" << std::string(70, '-') << "===//\033[0m" << std::endl;
+    //===----------------------------------------------------------------------===//
+    {
+        {
+            std::cout << "\033[31m//    Copy constructor test\033[0m" << std::endl;
+            MutantStack<int> mstack;
+            mstack.push(42);
+            mstack.push(43);
+            mstack.push(44);
+            for (MutantStack<int>::const_iterator cur = mstack.begin(); cur != mstack.end(); cur++) {
+                std::cout << *cur << " ";
+            }
+
+            std::cout << std::endl;
+            
+            MutantStack<int> mStackCopy(mstack);
+            for (MutantStack<int>::const_iterator cur = mStackCopy.begin(); cur != mStackCopy.end(); cur++) {
+                std::cout << *cur << " ";
+            }
+
+            std::cout << std::endl;
+        }
+        {
+            std::cout << "\033[31m//    Assignment Operator Overload test\033[0m" << std::endl;
+            MutantStack<int> mstack;
+            mstack.push(42);
+            mstack.push(43);
+            mstack.push(44);
+            for (MutantStack<int>::const_iterator cur = mstack.begin(); cur != mstack.end(); cur++) {
+                std::cout << *cur << " ";
+            }
+
+            std::cout << std::endl;
+            
+            MutantStack<int> mStackCopy = mstack;
+            for (MutantStack<int>::const_iterator cur = mStackCopy.begin(); cur != mStackCopy.end(); cur++) {
+                std::cout << *cur << " ";
+            }
+
+            std::cout << std::endl;
+        }
+        {
+            // Will not compile as const iterators are read only:
+            /*
+            MutantStack<int> mstack;
+            mstack.push(42);
+            mstack.push(43);
+            mstack.push(44);
+            
+            for (MutantStack<int>::const_iterator cur = mstack.begin(); cur != mstack.end(); cur++) {
+                (*cur)++;
+            }
+            */
+        }
+    }
     return 0;
 }
