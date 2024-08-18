927ff52815108723357d3e925910af2c52c1dd43
Ismaël MEHDID <ismaelmehdidwork@gmail.com>
2024-08-18 16:01:32 +0200

    add ex02 of cpp08

---
+++ cpp08/ex02/include/MutantStack.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        typedef typename std::deque<T>::iterator iterator;
+        typedef typename std::deque<T>::const_iterator const_iterator;
+
+        iterator begin() {
+            return this->c.begin();
+        }
+
+        const_iterator begin() const {
+            return this->c.begin();
+        }
+
+        iterator end() {
+            return this->c.end();
+        }
+
+        const_iterator end() const {
+            return this->c.end();
+        }
+};
